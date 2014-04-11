954634c36f641438489a3be3cb23c39643eaaa47
Pierre GIRAUD <pierre.giraud@camptocamp.com>
2014-04-11 10:35:14 +0200

    Get route from OSRM service

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var map;
 
+var OSRM_PRECISION = 6;
+
 var options = {
     controls: [
         new OpenLayers.Control.Attribution(),
@@ -10,16 +12,19 @@ var options = {
         })
     ],
     theme: null,
-    layers: [new OpenLayers.Layer.OSM()]
+    layers: [new OpenLayers.Layer.OSM(
+        null,
+        ['http://a.tiles.mapbox.com/v3/dennisl.map-dfbkqsr2/${z}/${x}/${y}.png']
+    )]
 };
 
 map = new OpenLayers.Map('map',options);
 
 map.setCenter(
-    new OpenLayers.LonLat(7, 46.472).transform(
+    new OpenLayers.LonLat(6.547, 46.572).transform(
         new OpenLayers.Projection("EPSG:4326"),
         map.getProjectionObject()
-    ),11
+    ),12
 );
 
 
@@ -78,24 +83,26 @@ layer.events.on({
         } else {
             trackPoints.push(obj.feature.geometry);
         }
-        // remove any existing track
-        if (track) {
-            layer.removeFeatures([track]);
-        }
         if (trackPoints.length >= 2) {
-            var line = new OpenLayers.Geometry.LineString(trackPoints);
-            track = new OpenLayers.Feature.Vector(line);
-            layer.addFeatures([track]);
-
             getRoute();
         }
     },
     'featuremodified': function(obj) {
-        if (track) {
-            layer.drawFeature(track);
+        if (trackPoints.length >= 2) {
+            getRoute();
         }
     }
 });
+
+function drawRoute(points) {
+    // remove any existing track
+    if (track) {
+        layer.removeFeatures([track]);
+    }
+    var line = new OpenLayers.Geometry.LineString(points);
+    track = new OpenLayers.Feature.Vector(line);
+    layer.addFeatures([track]);
+}
 
 var HoverFeatureControl = OpenLayers.Class(OpenLayers.Control, {
     initialize: function(layer, options) {
@@ -183,27 +190,78 @@ snappingControl.activate();
 var protocol = new OpenLayers.Protocol.Script({
     callbackKey: 'jsonp',
     callback: function(request) {
-        var feature = format.read(request.data.route_geometry);
-        console.log(feature.geometry);
-        //layer.addFeatures([feature]);
+        var decoded = decodeGeometry(request.data.route_geometry, OSRM_PRECISION);
+        var lonlats = decoded.lonlat;
+        var points = [];
+        console.log(lonlats[0], lonlats[lonlats.length - 1]);
+        for (var i = 0; i < lonlats.length; i++) {
+            var lonlat = lonlats[i];
+            var point = new OpenLayers.Geometry.Point(lonlat[1], lonlat[0]);
+            points.push(point.transform('EPSG:4326', map.getProjection()));
+        }
+        drawRoute(points);
+        //layer.addFeatures([new OpenLayers.Feature.Vector(line)]);
     }
 });
-var format = new OpenLayers.Format.EncodedPolyline();
 // Calls the OSRM service
 function getRoute() {
     var locs = [];
     for (var i=0; i < trackPoints.length; i++) {
-        var pt = trackPoints[i].clone().transform('EPSG:3857', 'EPSG:4326')
-        locs.push([pt.x, pt.y].join(','));
+        var pt = trackPoints[i].clone().transform(map.getProjection(), 'EPSG:4326')
+        // Note: loc=lat,lon
+        locs.push([pt.y.toPrecision(6), pt.x.toPrecision(6)].join(','));
     }
+    console.log(locs);
     locs = locs.join('&loc=');
     locs = "?loc=" + locs;
     protocol.read({
         url: "http://schweizmobil-r2014.gis.internal/osrm/viaroute" + locs,
         params: {
             instructions: false,
+            elevation: true,
             output: 'json',
-            z: 11
+            z: map.getZoom()
         }
     });
+}
+
+function decodeGeometry(encoded, precision) {
+    //decode compressed route geometry
+    precision = Math.pow(10, -precision);
+    var len = encoded.length, index = 0, lat = 0, lng = 0, ele = 0, array = [];
+    var ele_array = [];
+    while (index < len) {
+        var b, shift = 0, result = 0;
+        do {
+            b = encoded.charCodeAt(index++) - 63;
+            result |= (b & 0x1f) << shift;
+            shift += 5;
+        } while (b >= 0x20);
+        var dlat = ((result & 1) ? ~(result >> 1) : (result >> 1));
+        lat += dlat;
+        shift = 0;
+        result = 0;
+        do {
+            b = encoded.charCodeAt(index++) - 63;
+            result |= (b & 0x1f) << shift;
+            shift += 5;
+        } while (b >= 0x20);
+        var dlng = ((result & 1) ? ~(result >> 1) : (result >> 1));
+        lng += dlng;
+
+        shift = 0;
+        result = 0;
+        do {
+            b = encoded.charCodeAt(index++) - 63;
+            result |= (b & 0x1f) << shift;
+            shift += 5;
+        } while (b >= 0x20);
+        var dele = ((result & 1) ? ~(result >> 1) : (result >> 1));
+        ele += dele;
+        ele_array.push( ele * precision * 1000 );
+
+        array.push( [lat * precision, lng * precision] );
+    }
+    //console.log(ele_array);
+    return {'lonlat': array, 'ele': ele_array};
 }
